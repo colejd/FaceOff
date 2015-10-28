1ca1079412e955dae74ac11f9c44134953e7a4cb
seieibob <demonllama4>
2015-10-28 16:19:22 -0400

    Add headers for PS3EyeDriver

---
+++ Frameworks/PS3EyeDriver/PS3EyeDriverPriv.hpp
@@ -0,0 +1,16 @@
+//
+//  PS3EyeDriverPriv.h
+//  PS3EyeDriver
+//
+//  Created by Jonathan Cole on 10/23/15.
+//  Copyright © 2015 ProofOfConcept. All rights reserved.
+//
+
+#ifndef PS3EyeDriverPriv_h
+#define PS3EyeDriverPriv_h
+/* The classes below are not exported */
+
+#pragma GCC visibility push(hidden)
+
+#pragma GCC visibility pop
+#endif /* PS3EyeDriverPriv_h */
